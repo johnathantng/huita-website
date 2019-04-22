58055a92f77f2b45bcf6ea2faa4a0caef24a23a1
johnathantng <johnathan.tng@gmail.com>
2019-04-21 23:56:09 -0700

    finished boilerplate code

---
+++ src/containers/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+
+class App extends Component {
+	render() {
+		return(
+			<Navbar />
+			/* 
+				<Landing />
+				<Mission />
+				<Works />
+				<Services />
+				<Contact /> 
+			*/
+		);
+	}
+}
+
+export default App;
