eb0273cb22bfb2e42c9e5934b65aec3eb4b7bc0c
johnathantng <johnathan.tng@gmail.com>
2019-04-24 21:18:35 -0700

    Updates

---
+++ precache-manifest.540cee539ea1083fa2cd302a4d09751b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "5940f5863bdbb3602b15cb22778464eb",
+    "url": "/huita-website/static/media/iconsample1.5940f586.png"
+  },
+  {
+    "revision": "be0cda0b6859fee56f2d",
+    "url": "/huita-website/static/css/main.e469766f.chunk.css"
+  },
+  {
+    "revision": "0e5535d4c951e50de1d6",
+    "url": "/huita-website/static/js/runtime~main.96f15709.js"
+  },
+  {
+    "revision": "49d8c68793f81a6e9d74",
+    "url": "/huita-website/static/js/2.3a5cce55.chunk.js"
+  },
+  {
+    "revision": "22063450cfc9b958538df022f9dbe901",
+    "url": "/huita-website/static/media/huita-open.22063450.png"
+  },
+  {
+    "revision": "859130e4953eb874bb0e1bcfc9ceaa98",
+    "url": "/huita-website/static/media/mission.859130e4.png"
+  },
+  {
+    "revision": "be0cda0b6859fee56f2d",
+    "url": "/huita-website/static/js/main.e94b7b3a.chunk.js"
+  },
+  {
+    "revision": "90cfd2e4564fbba7bb5c23c5e0f52a1a",
+    "url": "/huita-website/static/media/iconsample2.90cfd2e4.png"
+  },
+  {
+    "revision": "1f765a286100beba907a2250425d2c1c",
+    "url": "/huita-website/static/media/iconsample3.1f765a28.png"
+  },
+  {
+    "revision": "2aee4232269b037ac317207f1609a445",
+    "url": "/huita-website/static/media/iconsample4.2aee4232.png"
+  },
+  {
+    "revision": "90de13cb21f3f7e40324089ddb5bbf09",
+    "url": "/huita-website/static/media/services.90de13cb.png"
+  },
+  {
+    "revision": "e2cefba3b4806ea39da356b106cced53",
+    "url": "/huita-website/static/media/connected.e2cefba3.png"
+  },
+  {
+    "revision": "7e84a154afe8c3f669da9a043ea47eb7",
+    "url": "/huita-website/index.html"
+  }
+];
